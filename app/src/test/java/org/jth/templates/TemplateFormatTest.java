0d439de7604268da90248d9083df38a84576fef8
Gabrian Mak <gabrianmak@gmail.com>
2018-11-12 21:15:54 -0500

    Adding format test

---
+++ app/src/test/java/org/jth/templates/TemplateFormatTest.java
@@ -0,0 +1,13 @@
+package org.jth.templates;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class TemplateFormatTest {
+
+    @Test
+    @DisplayName("Initializing FieldList")
+    public void testFieldList() {
+      TemplateFormat tf = new TemplateFormat();
+    }
+}
