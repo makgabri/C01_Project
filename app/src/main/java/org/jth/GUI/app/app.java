af1708a17acabb9370202feb6723c4c1ba38b095
TylarZhu <zhuxingyuan123@gmail.com>
2018-11-16 11:03:20 -0500

    add GUI into packge

---
+++ app/src/main/java/org/jth/GUI/app/app.java
@@ -0,0 +1,13 @@
+package org.jth.GUI.app;
+
+import com.userInterface.Windows.Windows;
+
+public class app {
+    public static void main(String[] args) {
+        displayStarterPage();
+    }
+
+    public static void displayStarterPage() {
+        Windows windowsPage = new Windows();
+    }
+}
