f503e0e179ba54ee46ee75451bf49fc0153f4d00
makgabri <gabrianmak@gmail.com>
2018-11-25 21:58:11 -0500

    Small commit

---
+++ app/src/main/java/org/jth/GUI/app/Tracker.java
@@ -0,0 +1,48 @@
+package org.jth.GUI.app;
+
+import java.util.HashMap;
+
+public class Tracker {
+
+    private HashMap<String, Object> windowHolder;
+    
+    // Declare instance, only one instance should exist at a time
+    private static Tracker instance = null;
+    private static boolean systemAlive = true;
+    
+    // Constructor
+    private Tracker() {
+        this.windowHolder = new HashMap<String, Object>();
+    }
+    
+    /**
+     * Gets the instance
+     * @return - instance of tracker
+     */
+    public static Tracker getInstance() {
+        if (instance == null) {
+            instance = new Tracker();
+        }
+        return instance;
+    }
+    
+    /**
+     * Adds the window object to array holder
+     * @param window - window object to be added to holder
+     */
+    public void addWindow(String name, Object window) {
+        windowHolder.put(name, window);
+    }
+    
+    public Object getWindow(String name) {
+        return windowHolder.get(name);
+    }
+    
+    public boolean getStatus() {
+        return systemAlive;
+    }
+    
+    public void closeSystem() {
+        systemAlive = false;
+    }
+}
